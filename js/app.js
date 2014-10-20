6c0e60a2be30b62c6c22fc925f5b71220649fe48
nathan <npjarvis@uw.edu>
2014-10-20 12:17:01 -0700

    did everything

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
  app.js
  application script for the JavaScript and Forms Demo
  */
-
+// strict mode helps avoid mis-spelling, instead of creating a new mis-spelled variable it will tell us we mis-spelled
 "use strict";
 
 /* onReady()
@@ -13,12 +13,39 @@
  * */
 function onReady() {
     //get a reference to the form
-
+    var ageForm = document.getElementById('age-form');
     //add an event listener for the 'submit' event passing onSubmit as the event handler function
+    ageForm.addEventListener('submit', onSubmit);
+
+    if (window.localStorage) {
+        ageForm.elements['name'].value = window.localStorage.getItem('defaultName');
+    }
 
     //add an event listener for the 'click' event on the exit button
     //for this one we will use an inline anonymous function so that you can get used to those
+    var exitButton = document.getElementById('exit-button'); // 'exit-button' is in html.index, then must addEventListener
+    // just pass function w/o name, put function in-line
+    // window object is used to re-direct web page
+    exitButton.addEventListener('click', function() {
+        // window.confirm gives user a confirm or deny option and returns true or false respectively
+        if (window.confirm('Are you realllllllly sure you want to leave? I worked my butt off for this!')){
+            window.location = 'https://www.google.com';
+        }
+    });
 
+    var resetButton = document.getElementById('reset-button');
+    resetButton.addEventListener('click', function () {
+       document.getElementById('age-message').style.display = 'none';
+    });
+
+    var nameInput = ageForm.elements['name'];
+    nameInput.addEventListener('change', function() {
+       if (window.localStorage) {
+           window.localStorage.setItem('defaultName', this.value);
+       }
+    });
+
+    // javascript DOM events, remove 'on' in front of events because its only for html
 } //onReady()
 
 //call onReady() when the DOMContentLoaded event is raised
@@ -38,9 +65,21 @@ function onSubmit(eventObject) {
     //remember that 'this' refers to the object that raised the event (i.e., the form)
 
     //get the name and the date-of-birth value
+    var name = this.elements['name'].value;
+    var dob = this.elements['dob'].value;
+
+    console.log(dob);
 
     //calculate the age
+    try {
+        var age =calculateAge(dob);
 
+        //display name and age
+        displayAge(name, age);
+    }
+    catch (exception) {
+        displayError(exception);
+    }
     //display the name and age
 
     //if the event object has a method called preventDefault,
@@ -69,7 +108,25 @@ function onSubmit(eventObject) {
  *   age in years [number]
  */
 function calculateAge(dob) {
+    // if dob is empty string or null string it is false, otherwise its true
+    if (!dob) {
+        throw new Error('Please tell me when you were born!');
+    }
     //calculate the person's age based on the date-of-birth
+    /*
+    var today = new Date();
+    dob = new Date(dob);
+    var yearsDiff = today.getFullYear() - dob.getUTCFullYear();
+    var monthDiff = today.getMonth() - dob.getUTCMonth();
+    var daysDiff = today.getDay() - dob.getUTCDay();
+
+    if (monthDiff < 0 || (0 === monthDiff && daysDiff < 0)) {
+        yearsDiff--;
+    }
+
+    return yearsDiff;
+    */
+    return moment().diff(dob, 'years');
 
 } //calculateAge()
 
@@ -81,8 +138,13 @@ function calculateAge(dob) {
  *   age - [number or string] age of person
  * */
 function displayAge(name, age) {
+    // test name, throw if not a name
+    var nameRegEx = new RegExp('^\\D+$');
+    if (!nameRegEx.test(name)) {
+        throw new Error('Your name cannot contain numbers!');
+    }
     //use displayMessage() to display the name and age
-
+    displayMessage(name + ', you are ' + age +' years old!');
 } //displayAge()
 
 /* displayAge()
@@ -93,7 +155,7 @@ function displayAge(name, age) {
  * */
 function displayError(error) {
     //use displayMessage to display the error
-
+    displayMessage(error, true);
 } //displayError()
 
 /* displayMessage()
@@ -104,5 +166,10 @@ function displayError(error) {
  *   isError - [boolean, default=false] set to true if this is an error message
  * */
 function displayMessage(message, isError) {
-
+    var msgElem = document.getElementById('age-message');
+    msgElem.innerHTML = message;
+    // read as a combo of if/then statement and an assignment in one line; assign the following value to classname,
+    // if yes alert alert-danger, else alert alert-success
+    msgElem.className = isError ? 'alert alert-danger' : 'alert alert-success';
+    msgElem.style.display = 'block';
 } //displayMessage()
